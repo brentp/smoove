866e32ab0a2ceb00eede99088b7f5290d25db52b
Brent Pedersen <bpederse@gmail.com>
2018-03-28 11:56:13 -0600

    fix rm

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -2,7 +2,7 @@
 
 cp ./smoove $(which smoove)
 
-rm -f to
+rm -rf to
 mkdir to
 REF=tests/subset.fa.gz
 export TMPDIR=`pwd` && smoove call -o to --processes 2 -x --genotype --fasta $REF --name NA24385-svs-cram --excludechroms '~^GL,~^HLA,~_random,~^chrUn,~alt,~decoy' tests/NA24385_chr1.cram
